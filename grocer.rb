77ed5403972190aee57dc026cfb6acbd182def16
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 19:47:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,18 @@
 require "pry"
 
+cart = [
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"AVOCADO" => {:price => 3.0, :clearance => true }},
+  {"KALE"    => {:price => 3.0, :clearance => false}}
+]
+
+
 def consolidate_cart(cart)
   binding.pry
   cart
 end
+
+puts consolidate_cart(cart)
 
 def apply_coupons(cart, coupons)
   # code here
