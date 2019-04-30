b5b8b261e822a2e20ea76929d142205f7a813dd2
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 21:55:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,12 @@ def apply_coupons(cart, coupons)
             :count => (item[:count] / coupon[:num])}
           }
       elsif item[:count] > 0 && item[:count] % coupon[:num] > 0
-        new_cart << {"#{item}" => {:price => item[:price], :clearance => item[:clearance], :count => item[:count] % coupon[:num]}}
+        new_cart << {"#{item}" => 
+          {
+            :price => item[:price], 
+            :clearance => item[:clearance], 
+            :count => item[:count] % coupon[:num]}
+          }
         new_cart << {"#{item} W/COUPON" =>
           {
             :price => coupon[:cost],
