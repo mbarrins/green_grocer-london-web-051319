807eab1651c1d0e1f5e3d9ba7978389eae01c0f8
Maija Barrins <maija.barrins@gmail.com>
2019-04-30 22:22:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,6 @@
-require "pry"
-
-cart = [
-  {"AVOCADO" => {:price => 3.0, :clearance => true }},
-  {"AVOCADO" => {:price => 3.0, :clearance => true }},
-  {"AVOCADO" => {:price => 3.0, :clearance => true }},
-  {"KALE"    => {:price => 3.0, :clearance => false}}
-]
-
-coupons = [{:item => "AVOCADO", :num => 2, :cost => 5.0}]
-
 def consolidate_cart(cart)
   cart.uniq.map{|items| items.map{|item, detail| [item, detail.merge({:count => cart.count(items)})]}.to_h}.inject(:merge).to_h
 end
-
-# puts consolidate_cart(cart).inspect
 
 def apply_coupons(cart, coupons)
   new_cart = {}
@@ -41,20 +28,14 @@ def apply_coupons(cart, coupons)
   new_cart
 end
 
-# puts apply_coupons(consolidate_cart(cart), coupons).inspect
-
 def apply_clearance(cart)
   cart.each do |item, details|
     details[:price] = (details[:price] * 0.80).round(2) if details[:clearance]
   end
 end
 
-# puts apply_clearance(consolidate_cart(cart)).inspect
-
 def checkout(cart, coupons)
   new_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
   total = new_cart.reduce(0){|sum, (item, details)| sum + (details[:count] * details[:price])}
   total > 100 ? (total * 0.9).round(2) : total
 end
-
-# puts checkout(cart, coupons)
